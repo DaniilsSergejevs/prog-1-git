f626f275a40dd3ec64cc85675c02afdb890cd430
mrdan <sergejev72@gmail.com>
2021-03-05 08:44:42 +0200

    Pievienots Main.py

---
+++ kvadratvienadojums.py
@@ -0,0 +1,16 @@
+import math
+
+a = float (input())
+b = float (input())
+c = float (input())
+discriminant = (pow(b, 2) - (4*a*c))
+print("Дискриминант D = %.2f" % discriminant)
+if discriminant > 0:
+    x1 = (-b + math.sqrt(discriminant)) / (2 * a)
+    x2 = (-b - math.sqrt(discriminant)) / (2 * a)
+    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+elif discriminant == 0:
+    x = -b / (2 * a)
+    print("x = %.2f" % x)
+else:
+    print("Nav saknus")
